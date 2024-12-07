12a2c760b2bbb93555d14cfe994f29970b36e198
hurrayPersimmon <rahel337@naver.com>
2024-12-07 16:02:36 +0900

    CT-184/Modify : test code delete

--- src/test/java/com/xeat/llmservice/LlmServiceApplicationTests.java
+++ src/test/java/com/xeat/llmservice/LlmServiceApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest(classes = LlmServiceApplicationTests.class)
 class LlmServiceApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+//	@Test
+//	void contextLoads() {
+//	}
 
 }
